a3fcc26cdac7e689103a4d21ad7d986c1227d967
Axel Rönnberg <axero912@student.liu.se>
2025-02-24 12:41:48 +0100

    mantine kanske

--- fad_frontend/src/App.tsx
+++ fad_frontend/src/App.tsx
@@ -1,14 +1,20 @@
 import './App.css'
 import AppRoutes from './navigation/routes.tsx'
+import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
+
 
 
 function App() {
   
 
   return (
+    <MantineProvider>
     <>
      <AppRoutes/>
     </>
+    </MantineProvider>
   )
 }
 
