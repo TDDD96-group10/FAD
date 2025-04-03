db67dbab65074a992f2fcc2a13f62fb80186dbfc
beki <baticbelmin10@gmail.com>
2025-04-03 21:10:12 +0200

    Added test for pipline

--- fad_backend/portal/tests/test_group.py
+++ fad_backend/portal/tests/test_group.py
@@ -17,3 +17,18 @@ class GroupModelTest(TestCase):
 
         self.assertEqual(len(group.users.all()), 1)
 
+    def test_create_group_and_add_user(self):
+        self.valid_attributes = {"size": ["m", "s", "l"]}
+        group = Group.objects.create(name="Test")
+        program = Program.objects.create(name="D-Sek", atributes=self.valid_attributes)
+        User.objects.create(user_id="belba436", role="admin", program=program, atributes={'size': "m"},group=group)
+        User.objects.create(user_id="belba437", role="admin", program=program, atributes={'size': "m"},group=group)
+        User.objects.create(user_id="belba438", role="admin", program=program, atributes={'size': "m"},group=group)
+
+        self.assertEqual(len(group.users.all()), 3)
+
+
+    
+
+
+
