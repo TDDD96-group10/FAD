0cca5ecc9dfed80adf26b3329173c16897750b38
rikardagren <rikag489@student.liu.se>
2025-04-14 13:20:28 +0200

    further work

--- fad_backend/portal/urls.py
+++ fad_backend/portal/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import HelloWorldView
+from .views import PostLinkView
 
 urlpatterns = [
     path("hello-world", HelloWorldView.as_view(), name="Hello World"),
+    path("post-link", PostLinkView.as_view(), name="Post Link"),
 ]
--- fad_backend/portal/views/post_link_view.py
+++ fad_backend/portal/views/post_link_view.py
@@ -1,21 +1,45 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from ..serializers.post_link_serializer import PostLinkSerializer
+#from ..serializers.post_link_serializer import PostLinkSerializer
 from rest_framework import status
+from rest_framework import serializers
+from drf_yasg.utils import swagger_auto_schema
+
+class PostLinkSerializer(serializers.Serializer):
+    title = serializers.CharField()
+    url = serializers.CharField()
+    author = serializers.IntegerField()
+    program = serializers.IntegerField()
+    send_notifcation = serializers.BooleanField()
+    text  = serializers.CharField()
 
 
 class PostLinkView(APIView):
    
-   """def get(self, request):
-        return Response()"""
+   #Vad gö
+   """@swagger_auto_schema(responses={200: PostLinkSerializer()}   )
+   def get(self, request):
+        return Response({
+            "user": {
+                "title": request.title,
+                "url": request.url,
+                "author": request.author,
+                "program": request.program,
+                "send_notification": request.send_notification,
+                "text" : request.text
+            }
+        })"""
    
-   def post(self, request):
-       #return Response()
 
+   @swagger_auto_schema(
+        request_body=PostLinkSerializer,
+        responses={201: PostLinkSerializer()}
+    )
+   def post(self, request):
      serializer = PostLinkSerializer(data=request.data)
      if serializer.is_valid():
-          # Typically, you'd save to the DB here, but we'll echo it back
-          return Response(serializer.data, status=status.HTTP_201_CREATED)
+        #Spara till databas
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
    
     
