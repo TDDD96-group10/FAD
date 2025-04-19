b5ad4f93d1b8711aa5b1be7fe8515b8ac8be3f30
AxelR <axel.ronnberg.25@gmail.com>
2025-04-19 19:37:47 +0200

    prefire pull request issues

--- fad_backend/portal/views/home_view.py
+++ fad_backend/portal/views/home_view.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from drf_yasg.utils import swagger_auto_schema
 
-# imports CLASS POST from models
 from ..models import Post
 from django.utils import timezone
 from portal.serializers.postSerializer import PostSerializer
@@ -13,9 +12,7 @@ class HomeView(APIView):
     @swagger_auto_schema(responses={200: PostSerializer(many=True)})
     def get(self, request):
 
-        # Get todays time
         today = timezone.now()
-        # Retrieve all post for today
         r = Post.objects.filter(start_time__gte=today).order_by("start_time")[:2]
         serializer = PostSerializer(r, many=True)
 
--- fad_backend/portal/views/share_info_view.py
+++ fad_backend/portal/views/share_info_view.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from drf_yasg.utils import swagger_auto_schema
 
-# imports CLASS POST from models
 from ..models import Post
 from django.utils import timezone
 from portal.serializers.textpostserializer import TextPostSerializer
@@ -19,13 +18,10 @@ class ShareView(APIView):
 
         serializer = TextPostSerializer(data=request.data)
         if serializer.is_valid():
-            # Check if the post is for today
             start_time = serializer.validated_data.get('start_time')
-            # Can't post things from the past
             if start_time and start_time.date() < today.date():
                 return Response({"error": "Post cant already have happend."}, status=status.HTTP_400_BAD_REQUEST)
 
-            # Save the post to the database
             new_post = Post(**serializer.validated_data)
             new_post.save()
         return Response("", status=status.HTTP_201_CREATED)
