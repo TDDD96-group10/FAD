2075b10d5174cc9783ea2bb33232c1ae20163306
beki1337 <36603112+beki1337@users.noreply.github.com>
2025-05-23 09:26:55 +0200

    Update login_view.py

--- fad_backend/security/views/login_view.py
+++ fad_backend/security/views/login_view.py
@@ -34,7 +34,7 @@ class LoginView(APIView):
         if serializer.is_valid():
             username = serializer.validated_data["username"]
             code = TwoFactorCode.retrieve_code(user_id=username)
-            sender = EmailSender("baticbelmin10@gmail.com", "cojz kldx nqwv diwf")
-            sender.send_email("belba436@student.liu.se", "T factor code", f"Your code is {code}")
+            sender = EmailSender("", "")
+            sender.send_email("@student.liu.se", "T factor code", f"Your code is {code}")
             return Response({"message": "The code is sent to the email"}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
